4b3bc661d85fcf1f622990fc0a9a258dbab61918
Ben Botsford <ben.botsford@flatironschool.com>
2022-10-28 13:42:57 -0500

    remove db directory

--- server/app.py
+++ server/app.py
@@ -5,7 +5,7 @@ from flask_migrate import Migrate
 from models import db, Message
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
