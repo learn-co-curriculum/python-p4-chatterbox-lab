4c276e3d8da298bb9e099de240f38ee585e1a305
Ben Botsford <ben.botsford@flatironschool.com>
2023-01-30 13:26:47 -0600

    fixes #4

--- server/models.py
+++ server/models.py
@@ -7,10 +7,3 @@ class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
 
     id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.String)
-    username = db.Column(db.String)
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
-
-    def __repr__(self):
-        return f'<Message by {self.username}: {self.body[:10]}...>'
