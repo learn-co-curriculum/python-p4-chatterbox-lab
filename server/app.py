7a5fd6b18391bde0c639b3bbaf927cde69b9df1c
Shanley Caswell <shanleycaswell@gmail.com>
2023-07-26 16:54:38 -0400

    added GET but not restful GET

--- server/app.py
+++ server/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
+from flask_restful import API, Resource
+
 
 from models import db, Message
 
@@ -14,9 +16,26 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+app=API(app)
+
 @app.route('/messages')
 def messages():
-    return ''
+    message_list = []
+    messages_ordered = Message.query.order_by(Message.created_at).all()
+
+    for message in messages_ordered:
+        message_dict = {
+            "id": message.id,
+            "body": message.body,
+            "username": message.username,
+            "created_at": message.created_at,
+            "updated_at": message.updated_at
+        }
+        message_list.append(message_dict)
+    
+    response = make_response(message_list, 200)
+
+    return response
 
 @app.route('/messages/<int:id>')
 def messages_by_id(id):
--- server/models.py
+++ server/models.py
@@ -12,3 +12,7 @@ class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
 
     id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String)
+    username = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
