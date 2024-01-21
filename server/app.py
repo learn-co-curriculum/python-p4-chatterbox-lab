73a543630169ece1b91e4d357168e0f336f28a29
Shadrack kipkorir <shadybett540@gmail.com>
2024-01-21 12:09:51 +0300

    added a get method

--- server/app.py
+++ server/app.py
@@ -14,13 +14,27 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages',methods=['GET'])
 def messages():
-    return ''
+    messages=Message.query.order_by(Message.created_at.asc()).all
+    return jsonify([message.serialize() for message in messages])
 
-@app.route('/messages/<int:id>')
+app.route('/messages',methods=['POST'])
+def create_message():
+    data = request.get_json()
+    new_message = Message(body=data['body'], username=data['username'])
+    db.session.add(new_message)
+    db.session.commit()
+    return jsonify(new_message.serialize())
+
+
+@app.route('/messages/<int:id>', methods=['PATCH'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.get_or_404(id)
+    data = request.get_json()
+    message.body = data['body']
+    db.session.commit()
+    return jsonify(message.serialize())
 
 if __name__ == '__main__':
     app.run(port=5555)
