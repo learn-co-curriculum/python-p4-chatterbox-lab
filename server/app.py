af51d21c3f4c7c17d9204af4e6f7d6140893a55e
Shadrack kipkorir <shadybett540@gmail.com>
2024-01-21 14:10:25 +0300

    done

--- server/app.py
+++ server/app.py
@@ -1,25 +1,30 @@
-from flask import Flask, request, make_response, jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
-
 from models import db, Message
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
+app.compact = False  # Corrected the attribute name
 
 CORS(app)
 migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages',methods=['GET'])
-def messages():
-    messages=Message.query.order_by(Message.created_at.asc()).all
-    return jsonify([message.serialize() for message in messages])
+@app.route('/')
+def home():
+    return "Hello"
 
-app.route('/messages',methods=['POST'])
+@app.route('/messages', methods=['GET'])
+def get_messages():
+    messages = Message.query.order_by(Message.created_at.asc()).all()  # Corrected the function call
+    serialized_messages = [message.serialize() for message in messages]
+    return jsonify(serialized_messages)
+
+@app.route('/messages', methods=['POST'])
 def create_message():
     data = request.get_json()
     new_message = Message(body=data['body'], username=data['username'])
@@ -27,9 +32,8 @@ def create_message():
     db.session.commit()
     return jsonify(new_message.serialize())
 
-
 @app.route('/messages/<int:id>', methods=['PATCH'])
-def messages_by_id(id):
+def update_message(id):
     message = Message.query.get_or_404(id)
     data = request.get_json()
     message.body = data['body']
@@ -44,5 +48,4 @@ def delete_message(id):
     return jsonify({"message": "Message deleted successfully"})
 
 if __name__ == '__main__':
-
-    app.run(port=5555)
+    app.run(debug=False,port=5555)
